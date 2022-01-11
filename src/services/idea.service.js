6b56b30311571ea99746f58097ef14169cd81ee4
Mario Gaytan <mario.gaytan@fonyou.com>
2022-01-11 16:23:27 -0600

    adding services

---
+++ src/services/idea.service.js
@@ -0,0 +1,70 @@
+const BaseService = require("./base.service")
+let _ideaRepository = null;
+
+
+class IdeaService extends BaseService {
+
+    constructor({IdeaRepository}) {
+        super(IdeaRepository)
+        _ideaRepository = IdeaRepository
+    }
+
+    async getUserIdeas(author){
+        if (!author) {
+            let error = new Error()
+            error.status = 400;
+            error.message = "User id must be sent"
+            throw error
+        }
+
+        return await _ideaRepository.getUserIdeas(author)
+    }
+   
+
+    async upVoteIdea(ideaId){
+        if (!ideaId) {
+            const error = new Error()
+            error.status = 400;
+            error.message = "Idea id must be sent"
+            throw error
+        }
+
+        const idea = await _ideaRepository.get(ideaId)
+        if (!idea) {
+            const error = new Error()
+            error.status = 404;
+            error.message = "Idea does not exist"
+            throw error
+        }
+
+       idea.upvotes.push(true);
+
+       return await _ideaRepository.update(ideaId,{upvotes:idea.upvotes})
+    }
+
+
+    async downVoteIdea(ideaId){
+        if (!ideaId) {
+            const error = new Error()
+            error.status = 400;
+            error.message = "Idea id must be sent"
+            throw error
+        }
+
+        const idea = await _ideaRepository.get(ideaId)
+        if (!idea) {
+            const error = new Error()
+            error.status = 404;
+            error.message = "Idea does not exist"
+            throw error
+        }
+
+       idea.downvotes.push(true);
+
+       return await _ideaRepository.update(ideaId,{downvotes:idea.downvotes})
+    }
+   
+   
+}
+
+module.exports = IdeaService
--- src/startup/container.js
+++ src/startup/container.js
@@ -14,7 +14,7 @@ const config = require("../config");
 /**
  * Service layer
  */
-const { HomeService } = require("../services");
+const { HomeService, UserService, IdeaService, CommentService } = require("../services");
 /**
  * Controllers layer
  */
@@ -41,6 +41,9 @@ container
   })
   .register({
     HomeService: asClass(HomeService).singleton(),
+    UserService: asClass(UserService).singleton(),
+    IdeaService: asClass(IdeaService).singleton(),
+    CommentService: asClass(CommentService).singleton(),
   })
   .register({
     HomeController: asClass(HomeController.bind(HomeController)).singleton(),
